f3d38eca17b1d20ad4f13a63e2f14c2f3d8fb648
pornpong <pornpoi@tot.co.th>
2020-10-27 11:30:23 +0700

    xxx

--- App.js
+++ App.js
@@ -1,11 +1,21 @@
-/* eslint-disable semi */
 /* eslint-disable prettier/prettier */
-import React from 'react'
-import HomeScreen from './src/HomeScreen'
-import RegisterScreen from './src/RegisterScreen'
+import React from 'react';
+import { View, SafeAreaView, StatusBar } from 'react-native';
+
+import HomeScreen from './src/HomeScreen';
+import RegisterScreen from './src/RegisterScreen';
+
+import AppNavigator from './src/AppNavigator';
+import { NavigationContainer } from '@react-navigation/native';
 
 export default function App() {
   return (
-    <HomeScreen />
-  )
+    <NavigationContainer>
+      <View style={{ flex: 1 }}>
+        <StatusBar barStyle="dark-content" />
+        <SafeAreaView />
+        <AppNavigator />
+      </View>
+    </NavigationContainer>
+  );
 }
